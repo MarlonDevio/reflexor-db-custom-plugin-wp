28d91b50fa480b09059ac970628ed6e072cdd9ba
MarlonHamaekersPXL <mar.hamaekers@gmail.com>
2024-08-24 16:44:21 +0200

    not working

--- resources/scripts/scripts.ts
+++ resources/scripts/scripts.ts
@@ -1,34 +1,37 @@
 import '../styles/style.css';
-import { wasInstructies } from '../images/svgs/index.js';
+import { wasInstructies } from '../images/svgs/index';
 
 const washingInstructionsRow = document
   .querySelector('.woocommerce-product-attributes-item--attribute_pa_was-instructies')
   ?.querySelector('.woocommerce-product-attributes-item__value')
   ?.querySelector('p');
 
-function createIcons(toMatch) {
-  if (!toMatch) return null;
-
-  const imageElement = document.createElement('img');
+function createIcons(toMatch: { name: string; url: any } | undefined): HTMLImageElement {
+  const imageElement: HTMLImageElement = document.createElement('img');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
   imageElement.src = toMatch?.url;
-  imageElement.alt = toMatch?.name;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+  imageElement.alt = <string>toMatch?.name;
 
   return imageElement;
 }
 
-function generateImages(instructionContainer) {
-  return instructionContainer.textContent.split(', ').map((text) => {
+function generateImages(instructionContainer: HTMLElement): HTMLImageElement[] | undefined {
+  return instructionContainer.textContent?.split(', ').map((text) => {
     const matchingInstruction = wasInstructies.find((instruction) => instruction.name === text.trim());
 
     return createIcons(matchingInstruction);
   });
 }
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 const images = generateImages(washingInstructionsRow);
 
-washingInstructionsRow.textContent = ' ';
 const container = document.createElement('div');
 container.classList.add('flex', 'gap-2', 'w-full', 'h-full');
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 images.forEach((image) => {
   image.classList.add('w-20', 'h-20');
   container.appendChild(image);
@@ -37,5 +40,5 @@ images.forEach((image) => {
 // const summaryContainer = document.querySelector('.entry-summary-items');
 
 const tabDesc = document.getElementById('tab-description');
-tabDesc.appendChild(container);
+tabDesc?.appendChild(container);
 // summaryContainer.appendChild(container);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import copy from './.vite/copy';
+import checker from 'vite-plugin-checker';
 
 const ROOT = path.resolve('../../../');
 const BASE = __dirname.replace(ROOT, '');
@@ -13,7 +14,7 @@ export default defineConfig({
     outDir: `dist`,
     emptyOutDir: true,
     rollupOptions: {
-      input: ['resources/scripts/scripts.js', 'resources/styles/style.css'],
+      input: ['resources/scripts/scripts.ts', 'resources/styles/style.css'],
       output: {
         entryFileNames: '[hash].js',
         assetFileNames: '[hash].[ext]',
@@ -31,11 +32,12 @@ export default defineConfig({
     }),
     {
       name: 'php',
-      handleHotUpdate({file, server}) {
+      handleHotUpdate({ file, server }) {
         if (file.endsWith('.php')) {
-          server.ws.send({type: 'full-reload'});
+          server.ws.send({ type: 'full-reload' });
         }
       },
     },
+    checker({ typescript: true }),
   ],
 });
